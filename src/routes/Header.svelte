a3b29d42144193b298cc0e08755f8d2d3c040066
JemappeIIe <chernyshov.luka@gmail.com>
2025-03-23 20:40:30 +0100

    Merge with Header branch

---
+++ src/routes/Header.svelte
@@ -0,0 +1,30 @@
+<script>
+
+</script>
+
+<header>
+    <h1><a href="/">JemappeIIe</a></h1>
+    <p>Description...</p>
+    <nav>
+        <ul>
+            <li><a href="/">A</a></li>
+            <li><a href="/">B</a></li>
+            <li><a href="/">C</a></li>
+        </ul>
+    </nav>
+</header>
+
+<style>
+    header {
+        text-align: center;
+        padding: 10px 0px;
+        background-color: gray;
+    }
+    ul {
+        display: flex;
+        justify-content: center;
+        list-style: none;
+        padding: 0px;
+        gap: 10px;
+    }
+</style>
