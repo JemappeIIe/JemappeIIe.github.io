3f2ed8be145541ccc428544d92af2e8f1c2327d8
JemappeIIe <chernyshov.luka@gmail.com>
2025-03-24 12:53:16 +0100

    Update global css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <Header />
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to </h1>
 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa laborum, quo temporibus perferendis deserunt animi quod, consectetur iusto officiis necessitatibus incidunt delectus cupiditate, similique autem quam molestias illo repellendus nobis.</p>
 
 <style>
-    
+
 </style>
