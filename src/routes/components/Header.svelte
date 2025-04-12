7aaabf546b54fc676cdf782225e541fd6c74faec
JemappeIIe <chernyshov.luka@gmail.com>
2025-04-13 01:22:30 +0200

    Add base to anchors in the header

--- src/routes/components/Header.svelte
+++ src/routes/components/Header.svelte
@@ -1,12 +1,16 @@
+<script lang="ts">
+    import {base} from '$app/paths';
+</script>
+
 <header class="w-full h-fit p-8">
     <nav class="flex justify-between">
-        <a href="/" class="nav-element">Home</a>
+        <a href="{base}/" class="nav-element">Home</a>
         <ul class="flex gap-8 m-auto">
-            <li><a href="/" class="nav-element">About</a></li>
+            <li><a href="{base}/" class="nav-element">About</a></li>
             <div class="nav-break"></div>
-            <li><a href="/" class="nav-element">Projects</a></li>
+            <li><a href="{base}/" class="nav-element">Projects</a></li>
             <div class="nav-break"></div>
-            <li><a href="/" class="nav-element">Contact</a></li>
+            <li><a href="{base}/" class="nav-element">Contact</a></li>
         </ul>
         <button class="nav-element">Mode</button>
     </nav>
